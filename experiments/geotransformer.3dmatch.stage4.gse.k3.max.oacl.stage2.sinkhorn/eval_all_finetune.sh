8c0e8ac5e85f551f1bd40bf3963771079899e0a0
Gardlin <2234965598@qq.com>
2023-06-29 12:05:39 +0800

    Update eval_all_finetune.sh

--- experiments/geotransformer.3dmatch.stage4.gse.k3.max.oacl.stage2.sinkhorn/eval_all_finetune.sh
+++ experiments/geotransformer.3dmatch.stage4.gse.k3.max.oacl.stage2.sinkhorn/eval_all_finetune.sh
@@ -5,8 +5,6 @@ for epoch in $(seq 10 20); do
 
       python eval.py --test_epoch=$epoch --benchmark=$1 --method=lgr --iteration=$n
 #      for k in 250 500 1000 2500 5000; do
-#  #      i=0
-#  #      len=3
 #        for m in $(seq 1 3);  do
 #            python eval.py --test_epoch=$epoch --benchmark=$1  --num_corr=$k --method=ransac --iteration=$n
 #          done
